d78916271371acded1fa6eb2551d3f7188843244
Henrietta Sharpe <henrietta_sharpe@yahoo.com>
2017-04-11 19:41:19 -0400

    tunes while you stir

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -8,6 +8,7 @@ recipeId = pulledId.splice(10,10).join("");
 
 
 
+
 var ingredientItm = "";
 //search for recipe 
    $.ajax({
@@ -21,16 +22,20 @@ var ingredientItm = "";
     })
 
 
+
+
   //after ajax request is complete
     .done(function (response2) {
+        var groceryLst;
+        var alreadyListed;
         console.log(response2);
          
-         var imguRL = response2[0].image;
+         var imgUrl = response2[0].image;
          var title = response2[0].title;
          var lstStep = response2[0].analyzedInstructions[0].steps;
          //display image and title
-         $(".card-content").html("<h6>" + "<center>" + title + "</cemter>" + "</h6>");
-         $(".card-image").html("<img src='" + imguRL + "'>");
+         $(".card-content").html("<h6>" + "<center>" + title + "</center>" + "</h6>");
+         $(".card-image").html("<img id=recipeImg src='" + imgUrl + "'>");
          
          
 
@@ -45,17 +50,33 @@ var ingredientItm = "";
        //list ingredients
        var ingredientLst = response2[0].extendedIngredients;
        for (var i=0; i<ingredientLst.length; i++){
+          var imgUrl = ingredientLst[i].image;
+          var ingredientImg = ("<img src='" + imgUrl + "'>")
+          console.log(ingredientImg);
 
-         $("#ingredients").append("<tr>" + "<td>" + "<input type='checkbox' id= 'checkItem" + [i] + "'/><label for='checkItem" + [i] + "'>" + ingredientLst[i].originalString + "</label>" +"</td>");
+         $("#ingredients").append("<tr>" + "<td>" + "<input type='checkbox' id= 'checkItem" + [i] + "'/><label for='checkItem" + [i] + "'>" + ingredientLst[i].originalString + "</label></td>");
+        
         }
 
 
        //list grocery list items
-       var groceryLst = response2[0].extendedIngredients;
+       groceryLst = response2[0].extendedIngredients;
+       alreadyListed = [];
+
        for (var i=0; i<groceryLst.length; i++){
 
-         $("#groceries").append("<tr>" + "<td>" + "<input type='checkbox' id= 'checkItem" + [i] + "'/><label for='checkItem" + [i] + "'>" + groceryLst[i].name + "</label>" +"</td>");
+          var imgUrl = groceryLst[i].image;
+          //if the grocery item was not listed already
+          if (alreadyListed.indexOf(imgUrl) < 0){
+            //add to alreadyListed array
+            alreadyListed.push(imgUrl);
+            //assign the img tag with the img url into a variable
+            var groceryImg = ("<img class = groceryItem src='" + imgUrl + "'>");
+          
+            //display grocery list with associated image.
+            $("#groceries").append("<tr>" + "<td>" + "<input type='checkbox' id= 'checkItemtwo" + [i] + "'/><label for='checkItemtwo" + [i] + "'>" + groceryLst[i].name + "</label>" +"</td><td>" + groceryImg + "</td>");
         }
+      }
     })
 
 
